e98d0102538032b79e9b35a33e97254063c74a70
Maciej Janiszewski <chleb@krojony.pl>
2016-11-30 23:23:49 +0100

    [Web] add tournaments APIs

--- wisielec/api/urls.py
+++ wisielec/api/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from views import user_api, ranking_api
+from views import user_api, ranking_api, tournament_api, tournament_invite_api
 
 urlpatterns = [
-    url(r'^user', user_api, name='user_api'),
-    url(r'^ranking', ranking_api, name='ranking_api'),
+    url(r'^user/(?P<username>.*)$', user_api, name='user_api'),
+    url(r'^user/', user_api, name='user_api'),
+    url(r'^ranking/', ranking_api, name='ranking_api'),
+    url(r'^tournament/invite/', tournament_invite_api, name='tournament_invite_api'),
+    url(r'^tournament/(?P<session_id>.*)$', tournament_api, name='tournament_api'),
+    url(r'^tournament/', tournament_api, name='tournament_api'),
 ]
--- wisielec/api/views.py
+++ wisielec/api/views.py
@@ -2,30 +2,73 @@
 import json
 from django.http import HttpResponse
 from profiles.models import UserProfile
+from game.models import Tournament
+from django.shortcuts import get_object_or_404
+
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def tournament_invite_api(request):
+    """
+    Invites user to tournament.
+    """
+    if request.POST:
+        tournament = get_object_or_404(Tournament, session_id=request.POST.get("tournament_id"))
+        user = get_object_or_404(UserProfile, username=request.POST.get("username"))
+        tournament.players.add(user)
+        response = {"result": "success", "session_id": tournament.session_id}
+    else:
+        response = {"result": "fail"}
+
+    return HttpResponse(json.dumps(response), content_type="application/json")
 
 
-def user_api(request):
+def tournament_api(request, session_id=None):
+    """
+    Returns list of tournaments for player.
+    """
+    if session_id:
+        tournaments = Tournament.objects.filter(session_id=session_id)
+    else:
+        tournaments = request.user.tournament_set.all()
+
+    response = {"tournaments": [{
+        "name": t.name,
+        "session_id": t.session_id,
+        "in_progress": t.in_progress,
+        "players": [x.username for x in t.players.all()],
+    } for t in tournaments]}
+
+    return HttpResponse(json.dumps(response), content_type="application/json")
+
+
+def user_api(request, username=None):
     """
     Returns currently logged in player.
     """
-    if request.user.is_authenticated():
+    if not username:
+        user = request.user
+    else:
+        user = get_object_or_404(UserProfile, username=username)
+
+    if user.is_authenticated():
         response = {
-            "username": request.user.username,
-            "authenticated": request.user.is_authenticated(),
+            "username": user.username,
+            "authenticated": user.is_authenticated(),
             "avatar": "",
-            "overall_score": request.user.score,
-            "ranking_score": request.user.ranking_score,
-            "position": request.user.ranking_position,
-            "won_games": request.user.won_games,
-            "lost_games": request.user.lost_games,
-            "won_tournaments": request.user.won_tournaments,
-            "lost_tournaments": request.user.lost_tournaments,
-            "tournaments": [x.session_id for x in request.user.tournament_set.all()],
+            "overall_score": user.score,
+            "ranking_score": user.ranking_score,
+            "position": user.ranking_position,
+            "won_games": user.won_games,
+            "lost_games": user.lost_games,
+            "won_tournaments": user.won_tournaments,
+            "lost_tournaments": user.lost_tournaments,
+            "tournaments": [x.session_id for x in user.tournament_set.all()],
         }
     else:
         response = {
             "username": "AnonymousUser",
-            "authenticated": request.user.is_authenticated(),
+            "authenticated": user.is_authenticated(),
             "avatar": "",
         }
     return HttpResponse(json.dumps(response), content_type="application/json")
