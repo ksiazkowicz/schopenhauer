efee518c74455446278aeea3054ee00038f69bea
Maciej Janiszewski <chleb@krojony.pl>
2016-12-30 20:59:32 +0100

    [Web] admin for userprofiles

--- wisielec/profiles/admin.py
+++ wisielec/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import UserProfile
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = UserProfile
+    list_display = ["username", "is_active", "is_superuser", "is_staff", "email", "score", "date_joined", "last_login", ]
+
+admin.site.register(UserProfile, UserAdmin)
