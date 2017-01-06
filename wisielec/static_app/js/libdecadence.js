d924213364c2708fab8ca529cec6c33f229fa0b9
Maciej Janiszewski <chleb@krojony.pl>
2017-01-06 18:11:10 +0100

    [Web] generate bootstrap alerts

--- wisielec/static_app/js/libdecadence.js
+++ wisielec/static_app/js/libdecadence.js
@@ -1,3 +1,5 @@
+var default_options = {"auto_dismissible": true, "dismiss_timeout": 4000};
+
 function update_players(block_id, players) {
     var players_list = document.getElementById(block_id);
     if (players != "") {
@@ -17,8 +19,63 @@ function show_info_block(error_level, string, options) {
     /*
         Shows notification block at the top of the page.
      */
-    // lol add it laters
-    alert(error_level + ": " + string);
+    // get date for alert ID
+    var d = new Date();
+
+    // prepare request
+    var data = {
+        "error_level": error_level, "string": string, "alert_id": d.getTime()
+    };
+    var template = "includes/decadence/alert_block.html";
+
+    // check if options are defined
+    if (options) {
+        if (options.auto_dismissible) {
+            // enable auto dismissing
+            $(function() {
+                setTimeout(function () {
+                    $("#alert-"+data.alert_id).alert('close');
+                }, options.dismiss_timeout);
+            });
+        }
+        if (options.has_button) {
+            data.has_button = true;
+            data.button_text = options.button_text;
+            data.button_href = options.button_href;
+        }
+        if (options.icon) {
+            data.icon = options.icon;
+        }
+        if (options.custom_template) {
+            // override template with provided in options
+            template = options.custom_template
+        }
+        if (options.extra_data) {
+            // if extra data is provided, replace data in data array
+            var extra_keys = Object.keys(options.extra_data);
+            for (var i=0; i<extra_keys.length; i++) {
+                data[extra_keys[i]] = options.extra_data[extra_keys[i]];
+            }
+        }
+    }
+
+    // add block
+    include_from_template("#body-alerts", template, data, "afterBegin");
+
+    // we might want to have that one
+    return data.alert_id
+}
+
+function fake_tournament_invite() {
+    var options = {
+        "has_button": true,
+        "button_text": "Dołącz!",
+        "button_href": "/game/t/test",
+        "icon": "fa fa-trophy",
+        "extra_data": { "tournament_name": "Fejkowe zaproszenie do turnieju", "username": "MrocznaPleśńŚmierci" },
+        "custom_template": "includes/decadence/alerts/tournament_invite.html"
+    };
+    show_info_block("info", "", options)
 }
 
 function include_from_template(selector, template, data, mode) {
@@ -46,7 +103,8 @@ function include_from_template(selector, template, data, mode) {
                 block.insertAdjacentHTML(mode, xhr.responseText)
             } else {
                 // stuff broke, show error
-                show_info_block("error", "Decadence failed to render template '"+template+"' with error '"+xhr.status+"'. Except stuff to be broken", false);
+                show_info_block("danger", "Decadence failed to render template '"+template+"' with error '"+xhr.status+"'. Except stuff to be broken",
+                default_options);
             }
         }
     };
