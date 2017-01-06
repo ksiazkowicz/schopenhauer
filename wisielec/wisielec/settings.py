b5b56b3e8c804d2135a5065d36696f1cec53e0dd
Maciej Janiszewski <chleb@krojony.pl>
2017-01-06 20:24:24 +0100

    [Web] production fix

--- wisielec/wisielec/settings.py
+++ wisielec/wisielec/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    """{
+    {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },"""
+    },
 ]
 
 AUTHENTICATION_BACKENDS = (
