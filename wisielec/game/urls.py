061658226889ded5ff57a0ef354933d1c48566ed
Maciej Janiszewski <chleb@krojony.pl>
2018-03-26 22:37:19 +0200

    login required, fix views

--- wisielec/game/urls.py
+++ wisielec/game/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
-from .views import lobby_view, new_game, current_game, tournament_view, new_tournament_view, tournament_invite_view
+from .views import lobby_view, new_game, current_game, tournament_view, \
+    tournament_invite_view
 
 urlpatterns = [
     url(r'^lobby', lobby_view, name='lobby_view'),
     url(r'^new_game', new_game, name='new_game_view'),
-    url(r'^new_tournament', new_tournament_view, name='new_tournament_view'),
+    url(r'^new_tournament', new_game, {"is_tournament": True},
+        name='new_tournament_view'),
     url(r'^g/(?P<session_id>.*)/$', current_game, name='game_view'),
     url(r'^t/(?P<session_id>.*)/$', tournament_view, name='tournament_view'),
-    url(r'^invite/(?P<username>.*)/$', tournament_invite_view, name='tournament_invite'),
+    url(r'^invite/(?P<username>.*)/$',
+        tournament_invite_view, name='tournament_invite'),
 ]
--- wisielec/game/views.py
+++ wisielec/game/views.py
@@ -4,41 +4,45 @@ from django.http import HttpResponseRedirect
 from game.models import Game, Tournament
 from profiles.models import UserProfile
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def lobby_view(request, template="game/lobby.html"):
     return render(request, template, locals())
 
 
-def new_game(request, template="new_game.html"):
-    return render(request, template, locals())
+@login_required
+def new_game(request, template="new_game.html", is_tournament=False):
+    return render(request, template, {"is_tournament": is_tournament})
 
 
-def new_tournament_view(request, template="new_game.html"):
-    is_tournament = True
-    return render(request, template, locals())
-
-
+@login_required
 def current_game(request, session_id, template="game/game.html"):
     game = get_object_or_404(Game, session_id=session_id)
     alphabet = u"aąbcćdeęfghijklłmnńoprsśtuówyzżź"
     return render(request, template, locals())
 
 
-def tournament_invite_view(request, username, template="tournament/invite.html"):
+@login_required
+def tournament_invite_view(request, username,
+                           template="tournament/invite.html"):
     tournaments = Tournament.objects.filter(in_progress=True)
     user = get_object_or_404(UserProfile, username=username)
 
     errors = []
     if request.POST:
-        tournament = get_object_or_404(Tournament, session_id=request.POST.get("tournament_id"))
+        tournament = get_object_or_404(
+            Tournament, session_id=request.POST.get("tournament_id"))
         tournament.players.add(user)
         return redirect("tournament_view", tournament.session_id)
 
     return render(request, template, locals())
 
 
-def tournament_view(request, session_id, template="tournament/tournament_lobby.html"):
+@login_required
+def tournament_view(request, session_id,
+                    template="tournament/tournament_lobby.html"):
     tournament = get_object_or_404(Tournament, session_id=session_id)
     coop_mode = "cooperation" in tournament.modifiers
     my_games = []
@@ -55,4 +59,3 @@ def tournament_view(request, session_id, template="tournament/tournament_lobby.h
             return HttpResponseRedirect("/game/t/%s" % tournament.session_id)
 
     return render(request, template, locals())
-
